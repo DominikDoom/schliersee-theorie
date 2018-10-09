b84d50aba92651cda25c9907a45ff7dee27ee71d
Dominik Reh <rehdominik@gmail.com>
2018-10-09 18:40:26 +0200

    Layout additions
    - Added a "General Information" section
    - Added Autoscroll from parallax to info section so that it covers the whole screen without showing the map or the top

--- util.js
+++ util.js
@@ -1,24 +1,41 @@
+var lastScrollTop = 0;
+$(document).on("scroll",function(){
+   var st = $(this).scrollTop();
+   if (st > lastScrollTop){
+	if ($(document).scrollTop() < 5) {
+		$([document.documentElement]).stop(true, false).animate({
+			scrollTop: $(".generalInfo").offset().top
+		}, 1000);
+	}
+   } else {
+      // upscroll code
+   }
+   lastScrollTop = st;
+});
+
 // Function to download data to a file
 function download(data, filename, type) {
-    var file = new Blob([data], {type: type});
-    if (window.navigator.msSaveOrOpenBlob) // IE10+
-        window.navigator.msSaveOrOpenBlob(file, filename);
-    else { // Others
-        var a = document.createElement("a"),
-                url = URL.createObjectURL(file);
-        a.href = url;
-        a.download = filename;
-        document.body.appendChild(a);
-        a.click();
-        setTimeout(function() {
-            document.body.removeChild(a);
-            window.URL.revokeObjectURL(url);  
-        }, 0); 
-    }
+	var file = new Blob([data], {
+		type: type
+	});
+	if (window.navigator.msSaveOrOpenBlob) // IE10+
+		window.navigator.msSaveOrOpenBlob(file, filename);
+	else { // Others
+		var a = document.createElement("a"),
+			url = URL.createObjectURL(file);
+		a.href = url;
+		a.download = filename;
+		document.body.appendChild(a);
+		a.click();
+		setTimeout(function () {
+			document.body.removeChild(a);
+			window.URL.revokeObjectURL(url);
+		}, 0);
+	}
 }
 
 // Scroll to top
-$(document).ready(function(){
+$(document).ready(function () {
 
 	// Der Button wird mit JavaScript erzeugt und vor dem Ende des body eingebunden.
 	var back_to_top_button = ['<a class="backToTop" href="#" onclick="return false;" style="display: none;">Zurück zum Seitenanfang</a>'].join("");
